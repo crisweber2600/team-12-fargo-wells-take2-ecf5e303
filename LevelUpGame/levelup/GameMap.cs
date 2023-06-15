c0c5e6ef42ad74cd59e4807ea202ec96fa602b3a
nicfigueroa <102761675+nicfigueroa@users.noreply.github.com>
2023-06-15 20:58:20 +0000

    Added GameMap and Position classes

---
+++ LevelUpGame/levelup/GameMap.cs
@@ -0,0 +1,35 @@
+namespace levelup
+{
+    public class GameMap
+    {
+        public Position[,]? positions { get; set; }
+        public Position? startingPosition { get; set; }
+
+        public GameMap()
+        {
+            //TODO  
+        }
+
+        private void CreatePositions()
+        {
+           //TODO
+        }
+
+        public virtual Position CalculateNewPosition(Position currentPosition, GameController.DIRECTION direction)
+        {
+             Position newPos = new Position(-1,-1);
+            //TODO
+
+            return newPos;
+        }
+
+        public bool IsPositionValid(Position pos)
+        {
+          
+            //TODO
+
+            return false;
+        }
+
+    }
+}
