b98db1f57fdef4f5c74caa20b9c5cd7b87d1a5e6
Your Name <smaa67886@gmail.com>
2023-06-16 15:26:34 +0000

    gamemap class finished!

--- LevelUpGame/levelup/GameMap.cs
+++ LevelUpGame/levelup/GameMap.cs
@@ -7,24 +7,62 @@ namespace levelup
 
         public GameMap()
         {
-            //TODO  
+            CreatePositions();//TODO  
         }
 
         private void CreatePositions()
         {
-            //TODO
+           positions = new Position[10,10];
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    Position pos = new Position(x, y);
+                    positions[x,y] = pos;
+                }
+            }
+            startingPosition = positions[0,0];
         }
 
         public virtual Position CalculateNewPosition(Position currentPosition, GameController.DIRECTION direction)
         {
-            Position newPos = new Position(-1, -1);
-            //TODO
+             Position newPos = new Position(-1,-1);
+            if(direction == GameController.DIRECTION.EAST)
+            {
+                newPos = new Position(currentPosition.x + 1, currentPosition.y);
+            }
+            else if (direction == GameController.DIRECTION.WEST)
+            {
+                newPos = new Position(currentPosition.x - 1, currentPosition.y);
+            }
+            else if (direction == GameController.DIRECTION.NORTH)
+            {
+                newPos = new Position(currentPosition.x, currentPosition.y + 1);
+            }
+            else if (direction == GameController.DIRECTION.SOUTH)
+            {
+                newPos = new Position(currentPosition.x, currentPosition.y - 1);
+            }
 
-            return newPos;
+            if ( IsPositionValid(newPos)) 
+            {
+                return newPos;
+            }
+            else 
+            {
+                return currentPosition;
+            }
         }
 
         public bool IsPositionValid(Position pos)
         {
+            if (pos.x >= 0 && pos.x < 10 && pos.y >=0 && pos.y < 10)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
             //TODO
 
             return false;
